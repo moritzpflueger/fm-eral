d13b63748c213e08fb6e71c251452dff3a2872bc
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-17 14:31:49 +0200

    polish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,14 @@ const Footer = () => {
         as natur arum is dolor re prorese quatium quatur atem nem
         nimagni volum faceptate et, que reper          
       </p>
-      <div className="flex justify-between">
+      <div className="flex flex-col sm:flex-row justify-between py-10 order-1">
         <a href="/legal">legal notice</a>
-        <a href="/impressum">impressum</a>
-      </div>
-      <div className="flex gap-5 items-center justify-center">
-        <div>Logo 1</div>
-        <div>Logo 2</div>
-        <div>Logo 3</div>
+        <div className="flex gap-5 items-center sm:justify-center order-3 sm:order-2">
+          <div>Logo 1</div>
+          <div>Logo 2</div>
+          <div>Logo 3</div>
+        </div>
+        <a href="/impressum" className="order-2 sm:order-3">impressum</a>
       </div>
     </footer>
   );
