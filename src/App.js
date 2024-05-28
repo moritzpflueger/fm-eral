60a59ba1942d6c4b62572e68f338c21d5d91885d
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-28 14:26:37 +0200

    make text centered

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="w-full flex flex-col items-center justify-center h-[100dvh]">
       <p 
-        className="p-10 max-w-2xl text-white"
+        className="p-10 max-w-2xl text-white text-center"
         style={{ fontFamily: 'Whyte, sans-serif' }}
       >
         CURRENTLY UNDER CONSTRUCTION  
