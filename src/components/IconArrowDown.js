599901f0d6ca3ed8025588f8a4e5db45dac10abf
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-29 16:53:44 +0200

    adjust arrow icon

--- src/components/IconArrowDown.js
+++ src/components/IconArrowDown.js
@@ -1,13 +1,11 @@
 const IconArrowDown = () => {
   return (
-    <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg">
-      {/* <!-- Vertical line of the arrow --> */}
-      <line x1="24" y1="0" x2="24" y2="48" stroke="black" strokeWidth="2"/>
-      {/* <!-- Left diagonal line of the arrow tip --> */}
-      <line x1="6" y1="28" x2="24" y2="48" stroke="black" strokeWidth="2"/>
-      {/* <!-- Right diagonal line of the arrow tip --> */}
-      <line x1="42" y1="28" x2="24" y2="48" stroke="black" strokeWidth="2"/>
-    </svg>    
+    <span 
+      className="text-7xl mt-5"
+      style={{ fontFamily: 'Synt, sans-serif' }}
+    >
+      &#x2193;
+    </span>
   )
 }
 
