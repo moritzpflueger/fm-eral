eb0e06f53be8a9f036c0d37477ab06fcb9a3a4ff
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-29 16:47:53 +0200

    introduce schedule carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import RadioPlayer from './components/RadioPlayer';
 import IconArrowDown from './components/IconArrowDown';
+import Schedule from './components/Schedule';
 import './App.css';
 
 function App() {
@@ -12,47 +13,7 @@ function App() {
       {/* <RadioPlayer /> */}
 
       <IconArrowDown />
-      <p 
-        className="p-10 max-w-2xl"
-        style={{ fontFamily: 'Whyte, sans-serif' }}
-      >
-        What exactly is translocality, and how does it connect with radio?
-        Translocality encompasses the interconnectedness and dynamic movement of ideas, people, and practices across diverse localities. It facilitates exchanges of knowledge, culture, and resources across various geographical, social, and cultural contexts.
-        <br />
-        <br />
-        Radio serves as a powerful illustration of translocality, reaching
-        out to diverse audiences across different geographical regions.
-        Through radio waves, information, ideas, and cultural expressions
-        transcend physical boundaries, resonating with communities and
-        bridging geographical distances. The medium of radio provides a
-        platform for the amplification of diverse voices and perspectives.
-        <br />
-        <br />
-        As we explore the concept of translocality further, we’ve innovatively transformed our studio into a mobile cargo space. This
-        adaptation allows us to showcase works and traces while simultaneously functioning as a mobile radio station. This dynamic approach transforms the presentation of works into a performa-
-        tive experience, engaging audiences in unconventional ways and
-        challenging conventional ideas of space and connection.      
-      </p>
-      <div className="relative w-full my-16 py-16 px-5 flex items-center justify-center">
-        <div
-          className="absolute inset-0 h-full w-full  bg-[radial-gradient(#000000_1px,transparent_1px)] [background-size:16px_16px]"
-        ></div>
-        <iframe 
-          src="https://calendar.google.com/calendar/embed?src=ac131be6ead9fcedc826ee06da7a9437d1c025cadd699196b0b3b184ae576e63%40group.calendar.google.com&ctz=Europe%2FBerlin" 
-          className="z-10"
-          style={{border: 0}}
-          width="800" 
-          height="600"
-          frameborder="0"
-          scrolling="no"
-        ></iframe>        
-      </div>
-      <div className="w-full my-32 px-5 flex items-center justify-center">
-        <iframe 
-          className="w-full max-w-4xl aspect-[5/6] border-2" 
-          src="https://docs.google.com/document/d/e/2PACX-1vT3BPAbMeSHgWqtz0cHxGBZnW6texpMgg56UsCv-oIeb43ri6Dd0g_o0bV4DPflVlQZJMaSiWMejubO/pub?embedded=true"
-        ></iframe>
-      </div>
+      <Schedule />
       <Footer />
     </div>
   );
---
+++ src/components/Schedule.js
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const Schedule = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div 
+        className="relative w-full max-w-3xl hidden sm:block border border-black"
+        style={{ fontFamily: 'Synt, sans-serif' }}
+      >
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          allowTouchMove={false}
+          navigation={
+            {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev'
+            }
+          }
+        >
+          <SwiperSlide className="flex items-center justify-center aspect-video">Desktop Schedule 1</SwiperSlide>
+          <SwiperSlide className="flex items-center justify-center aspect-video">Desktop Schedule 2</SwiperSlide>
+          <SwiperSlide className="flex items-center justify-center aspect-video">Desktop Schedule 3</SwiperSlide>
+        </Swiper>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>        
+      </div>
+      <div className="w-full px-5 sm:hidden">
+        <div className="flex items-center justify-center border border-black w-full mb-5 aspect-video">Mobile Schedule 1</div>
+        <div className="flex items-center justify-center border border-black w-full mb-5 aspect-video">Mobile Schedule 2</div>
+        <div className="flex items-center justify-center border border-black w-full mb-5 aspect-video">Mobile Schedule 3</div>
+      </div>
+    </div>
+  );
+}
+export default Schedule;
