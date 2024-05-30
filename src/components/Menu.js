3611f4cc35aa9a886b0072111ae422365862885d
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-30 15:28:26 +0200

    polish menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,5 @@
+import IconClose from './icons/IconClose';
+
 const Menu = ({ showMenu, handleMenuToggle }) => {
   
   const hideMenu = () => {
@@ -10,12 +12,18 @@ const Menu = ({ showMenu, handleMenuToggle }) => {
     <nav className={`fixed flex items-start justify-between w-full z-50`}>
       <div className="h-[100dvh]">
         <ul className="fixed w-full flex flex-col items-center gap-0 px-10 pt-32 pb-10 text-4xl sm:text-5xl top-0 left-0 bg-white z-20">
-          <li className="absolute top-3 right-3">
-            <button onClick={hideMenu}>
-              x close
+          <li className="absolute top-2 right-1">
+            <button 
+              onClick={hideMenu}
+              className="w-16 h-16"
+            >
+              <IconClose />
             </button>
           </li>
-          <li className="mt-0">
+          <li className="text-2xl">
+            FMeral.world
+          </li>
+          <li className="mt-16">
             <a href="#about" onClick={hideMenu}>
               About
             </a>
@@ -31,11 +39,15 @@ const Menu = ({ showMenu, handleMenuToggle }) => {
             </a>
           </li>
           <li className="mt-8">
+            <a href="https://www.instagram.com" onClick={hideMenu}>
+              Instagram
+            </a>
+          </li>
+          <li className="mt-16 mb-8 text-2xl flex gap-5">
             <a href="/impressum" onClick={hideMenu}>
               Impressum
             </a>
-          </li>
-          <li className="mt-8 mb-4">
+            /
             <a href="/legal" onClick={hideMenu}>
               Legal Notice
             </a>
