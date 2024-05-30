b0bb7afd20c3af8b53066419f0f341a9e2c17aa2
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-30 13:53:42 +0200

    make user icon a component

---
+++ src/components/icons/IconUser.js
@@ -0,0 +1,14 @@
+const IconUser = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 90 100" 
+      fill="currentColor" 
+      className="w-7 h-7 mr-2 text-fmBlue"
+    >
+      <path data-name="Path 321" d="M121.456,382.184v-.27c0-21.716-15.084-46.539-36.488-50.788,9.881-3.686,16.893-12.849,16.893-23.6a25.087,25.087,0,0,0-25.52-25.347,25.087,25.087,0,0,0-25.52,25.347c0,10.749,7.012,19.912,16.893,23.6-21.4,4.249-36.488,29.072-36.488,50.788v.27Z" transform="translate(-31.226 -282.182)"></path>
+    </svg>
+  );
+}
+
+export default IconUser;
