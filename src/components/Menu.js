5edc4a41ce9b211b15da00ea1a697b6efa781347
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-30 14:15:19 +0200

    polish menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,33 +9,33 @@ const Menu = ({ showMenu, handleMenuToggle }) => {
   return (
     <nav className={`fixed flex items-start justify-between w-full z-50`}>
       <div className="h-[100dvh]">
-        <ul className="fixed w-full flex flex-col items-center gap-0 px-10 pt-32 pb-10 text-5xl top-0 left-0 bg-white z-20">
+        <ul className="fixed w-full flex flex-col items-center gap-0 px-10 pt-32 pb-10 text-4xl sm:text-5xl top-0 left-0 bg-white z-20">
           <li className="absolute top-3 right-3">
             <button onClick={hideMenu}>
               x close
             </button>
           </li>
-          <li className="font-semibold mt-0">
+          <li className="mt-0">
             <a href="#about" onClick={hideMenu}>
               About
             </a>
           </li>
-          <li className="font-semibold mt-8">
+          <li className="mt-8">
             <a href="#schedule" onClick={hideMenu}>
               Schedule
             </a>
           </li>
-          <li className="font-semibold mt-8">
+          <li className="mt-8">
             <a href="#artists" onClick={hideMenu}>
               Artists
             </a>
           </li>
-          <li className="font-semibold mt-8">
+          <li className="mt-8">
             <a href="/impressum" onClick={hideMenu}>
               Impressum
             </a>
           </li>
-          <li className="font-semibold mt-8">
+          <li className="mt-8 mb-4">
             <a href="/legal" onClick={hideMenu}>
               Legal Notice
             </a>
