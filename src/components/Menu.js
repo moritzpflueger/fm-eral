46a5392a5640f707db11f20891117ef96f39e1f8
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-30 16:47:47 +0200

    open instagram in a new tab

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -40,7 +40,7 @@ const Menu = ({ showMenu, handleMenuToggle }) => {
             </Link>
           </li>
           <li className="mt-8" style={{ fontFamily: 'Synt, serif'}}>
-            <a href="https://www.instagram.com" onClick={hideMenu}>
+            <a href="https://www.instagram.com" target="_blank" onClick={hideMenu}>
               Instagram
             </a>
           </li>
