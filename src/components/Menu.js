4f7782281529a8584506ebea62a0552ca27355a9
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-31 12:57:56 +0200

    fix menu must be scrollable for small screens

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,8 +12,8 @@ const Menu = ({ showMenu, handleMenuToggle }) => {
   return (
     <nav className={`fixed flex items-start justify-between w-full z-50`}>
       <div className="h-[100dvh]">
-        <ul className="fixed w-full flex flex-col items-center gap-0 px-10 pt-32 pb-10 text-4xl sm:text-5xl top-0 left-0 bg-white z-20">
-          <li className="absolute top-2 right-1">
+        <ul className="fixed w-full flex flex-col items-center gap-0 px-10 py-6 text-4xl sm:text-5xl top-0 left-0 bg-white z-20 max-h-[100dvh] overflow-y-auto">
+          <li className="absolute top-3 right-1">
             <button 
               onClick={hideMenu}
               className="w-12 h-12"
