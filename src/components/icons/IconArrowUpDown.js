2d293346ba637e0a124ad4dd57ae0eaf2f71f538
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-05-31 17:50:55 +0200

    correct position of updown arrow

--- src/components/icons/IconArrowUpDown.js
+++ src/components/icons/IconArrowUpDown.js
@@ -1,7 +1,7 @@
 const IconArrowUpDown = () => {
   return (
     <span 
-      className="text-7xl my-10"
+      className="text-7xl my-10 -translate-y-4 sm:-translate-y-6"
       style={{ fontFamily: 'Synt, sans-serif' }}
     >
       &#x2195;
