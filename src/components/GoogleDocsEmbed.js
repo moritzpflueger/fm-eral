61447b95ae789fea4dce5c237a12146c07899888
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-06-01 14:23:07 +0200

    adjust google doc breakpoints

--- src/components/GoogleDocsEmbed.js
+++ src/components/GoogleDocsEmbed.js
@@ -4,32 +4,39 @@ const GoogleDocsEmbed = () => {
 
   const iframeRef = useRef(null);
 
+  const baseHeight = 9850; // Base height for widths >= 800px
+
   const breakpoints = [
-    { min: 775, max: 800, height: 31200 },
-    { min: 750, max: 775, height: 31100 },
-    { min: 725, max: 750, height: 31350 },
-    { min: 700, max: 725, height: 31500 },
-    { min: 670, max: 700, height: 31800 },
-    { min: 640, max: 670, height: 32100 },
-    { min: 620, max: 640, height: 31200 },
-    { min: 600, max: 620, height: 31400 },
-    { min: 575, max: 600, height: 31600 },
-    { min: 550, max: 575, height: 31800 },
-    { min: 525, max: 550, height: 32000 },
-    { min: 500, max: 525, height: 32200 },
-    { min: 475, max: 500, height: 32500 },
-    { min: 450, max: 475, height: 32800 },
-    { min: 425, max: 450, height: 32900 },
-    { min: 400, max: 425, height: 33000 },
+    { min: 775, max: 800, heightFactor: 1.02 },
+    { min: 750, max: 775, heightFactor: 1.025 },
+    { min: 725, max: 750, heightFactor: 1.045 },
+    { min: 700, max: 725, heightFactor: 1.055 },
+    { min: 670, max: 700, heightFactor: 1.077 },
+    { min: 640, max: 670, heightFactor: 1.095 },
+    { min: 620, max: 640, heightFactor: 1.025 },
+    { min: 600, max: 620, heightFactor: 1.04 },
+    { min: 575, max: 600, heightFactor: 1.05 },
+    { min: 550, max: 575, heightFactor: 1.055 },
+    { min: 525, max: 550, heightFactor: 1.08 },
+    { min: 500, max: 525, heightFactor: 1.095 },
+    { min: 475, max: 500, heightFactor: 1.115 },
+    { min: 450, max: 475, heightFactor: 1.15 },
+    { min: 425, max: 450, heightFactor: 1.16 },
+    { min: 400, max: 425, heightFactor: 1.18 },
+    { min: 390, max: 400, heightFactor: 1.22 },
+    { min: 380, max: 390, heightFactor: 1.23 },
+    { min: 370, max: 380, heightFactor: 1.245 },
+    { min: 360, max: 370, heightFactor: 1.26 },
+    { min: 300, max: 360, heightFactor: 1.3 },
   ];
 
   const calculateHeight = () => {
     const width = window.innerWidth;
-    let height = 30900; // Default height
+    let height = baseHeight; // Default height
 
     for (const breakpoint of breakpoints) {
       if (width >= breakpoint.min && width < breakpoint.max) {
-        height = breakpoint.height;
+        height = baseHeight * breakpoint.heightFactor;
         break;
       }
     }
@@ -65,7 +72,8 @@ const GoogleDocsEmbed = () => {
         <embed 
           ref={iframeRef}
           src="https://docs.google.com/document/d/e/2PACX-1vT3BPAbMeSHgWqtz0cHxGBZnW6texpMgg56UsCv-oIeb43ri6Dd0g_o0bV4DPflVlQZJMaSiWMejubO/pub?embedded=true" 
-          width="100%"></embed>
+          width="100%"
+        ></embed>
       </div>
     </section>
   );
