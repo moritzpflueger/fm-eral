07308fa79a5122316588b4c4cd610604b22d4b2b
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-06-05 11:48:28 +0200

    update footer layout again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,35 +5,34 @@ import IconZHdKLogo from './icons/IconZHdKLogo';
 
 const Footer = () => {
   return (
-    <footer className="bg-fmBlue text-white p-5 sm:p-10 pb-10">
+    <footer className="bg-fmBlue text-white p-10">
       <p>
         Curatorial concept and implementation: Kirill Agafonov, Florian Amoser, Jonas Bernetta, Sharon Chan, Alisha Dutt Islam, Nistiman Erdede, Hannah Essler, Micol Favini, Tsz Hei Fung, Christian Gieben, Emanuel Haab, Roni Idrizaj, Charles Kwong, Ludwig Lederer, Sébastien Mitra, Engy Mohsen, Hans-Jakob Mühlethaler, Jan Reimann, Hiba Tahhan, Soraya Tashima, Anastasia Tatarenko, Egor Tatarenko. Supervision and support: Nicole Frei, Simon Grab, Patrick Müller, Nicholas Schärer, Irene Vögeli       
       </p>
-      <div className="flex items-end justify-between mt-10 flex-wrap sm:flex-nowrap">
-        <div className="flex flex-col flex-wrap items-start justify-between">
-          <Link to="/legal" >legal notice</Link>
-          <Link to="/impressum" className="">impressum</Link>
-          <div className="flex items-center gap-5 mt-5 -translate-x-1">
+      <div className="flex flex-wrap items-center justify-between py-10 order-1 gap-5">
+        <Link to="/legal" >legal notice</Link>
+        <div className="flex mx-auto pt-8 sm:pt-0 gap-5 items-center sm:justify-center order-3 sm:order-2">
 
-            <a href="https://www.instagram.com/zhdk_transdisciplinary" target="_blank">
-              <IconInstagram />          
-            </a>
-            
-            <a href="https://soundcloud.com/kontakt-mtr" target="_blank">
-              <IconSoundcloud />
-            </a>
-
-
-          </div>
-        </div>
-        <div className='flex w-full mt-5 sm:mt-0 sm:justify-end'>
-          <a href="https://www.zhdk.ch/" target="_blank" className="">
-            <IconZHdKLogo />
+          <a href="https://www.instagram.com/zhdk_transdisciplinary" target="_blank">
+            <IconInstagram />          
           </a>
+
+          <a href="https://soundcloud.com/kontakt-mtr" target="_blank">
+            <IconSoundcloud />
+          </a>
+
+          {/* TODO: Add icon and link to ZHDK? */}
+          {/* https://www.zhdk.ch/ */}
+
         </div>
+        <Link to="/impressum" className="order-2 sm:order-3">impressum</Link>
       </div>
+      <div className='flex w-full mt-5 sm:mt-0 sm:justify-end'>
+        <a href="https://www.zhdk.ch/" target="_blank" className="">
+          <IconZHdKLogo />
+        </a>
+      </div>      
     </footer>
   );
 };
-
 export default Footer;
--- src/components/icons/IconZHdKLogo.js
+++ src/components/icons/IconZHdKLogo.js
@@ -5,7 +5,7 @@ const IconZHdKLogo = () => {
       xmlns="http://www.w3.org/2000/svg" 
       viewBox="0 0 1208 552"
       fill="currentColor"
-      className="w-48"
+      className="w-36"
     >
       <title>ZHdK_Logo</title>
       {/* <style>
